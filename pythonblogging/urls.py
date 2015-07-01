6e47fdcbffe037416ae8345970dfb9c044397a3b
judgegrubb <elijah.grubb@utah.edu>
2015-07-01 12:17:56 -0600

    made links more generic

--- pythonblogging/urls.py
+++ pythonblogging/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('blog.urls')),
+    url(r'^', include('blog.urls', namespace="blog")),
 ]
 
 admin.site.site_title = 'Blog Admin Interface'
